3c5ed9457e2a5a410ef0286a0b03ecfe14313fb6
Keshav <40142486+keshavsoft@users.noreply.github.com>
2022-10-14 10:42:33 +0530

    keshav

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    bundle: './src/js/main.js'
+  },
+  output: {
+    path: "C:\\KeshavSoft\\nodejs\\multi\\2022\\oct\\8\\KJson\\public\\js",
+    filename: '[name].js',
+  },
+};
